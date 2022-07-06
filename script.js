e2cb4f811e518734374c09706cbe0ba5807a4fe5
Valentina Busamia <valentinabusamia1@gmail.com>
2022-07-05 21:42:52 -0300

    desafio1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function sumar( numero1 = 0, numero2 = 0){
+    return numero1 + numero2
+}
+
+let resultado = sumar(8 , 7)
+console.log (resultado)
+
+const promedio = 15
+
+function dividir (promedio){
+    
+    promedio / 2
+}
+
+
+
